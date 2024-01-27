afd70866d472179b962f1af08c374bdc8c5dfc76
Husseinjan <puljahusein@gmail.com>
2024-01-27 12:17:09 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-between items-center  bg-gray-200 ">
+      <img src='logo.png' alt='logo' className='w-[14em] h-[7em] p-4'/>
+      <h1 className="text-2xl font-bold text-blue-700">Hello, Tailwind CSS!</h1>
+     
     </div>
   );
 }
